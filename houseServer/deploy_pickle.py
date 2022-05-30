6c577373d22980fc3d8e32182638b10b20bc9f5b
Nadriana100 <nadriana100@gmail.com>
2022-05-29 23:24:52 -0400

    sales price predictor working

--- houseServer/deploy_pickle.py
+++ houseServer/deploy_pickle.py
@@ -2,7 +2,7 @@
 from turtle import pd
 import numpy as np
 import pandas as pd
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import json 
 import pickle
 
@@ -12,13 +12,28 @@ app = Flask(__name__)
 model = pickle.load(open('finalized_model_2.pkl','rb'))
 with open('encoder.pkl', 'rb') as handle:
     enc = pickle.load(handle)
-@app.route('/predict')
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/predict', methods=['POST'])
 def predict():
     # Get the data from the POST request.
-    data = pd.read_json("data.json") #request.get_json(force=True)
+    #data = pd.read_json("data.json") #request.get_json(force=True)
+    r = request.form
+    data = pd.json_normalize(r)
+    print(data)
+    # data = pd.DataFrame(request.json)
+    data = data.astype ({
+                        "BedroomAbvGr": "int", 
+                        "FullBath": "int", 
+                        "LotArea": "int", 
+                        "OverallQual": "int", 
+                        "YearBuilt": "int"
+                        })
+   
     data = pd.concat([data.select_dtypes(exclude='object'),pd.DataFrame(enc.transform(data.select_dtypes('object')).toarray(),index=data.select_dtypes('object').index, columns=enc.get_feature_names().tolist())], axis =1)
-    # Make prediction using model loaded from disk as per the data.
-    prediction = model.predict(data.drop(columns="SalePrice"))
+    # # Make prediction using model loaded from disk as per the data.
+    prediction = model.predict(data)
 
     # Take the first value of prediction
     output = prediction
@@ -26,4 +41,6 @@ def predict():
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
+
 
